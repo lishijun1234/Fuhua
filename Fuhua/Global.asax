0bfd3f7a662c51ca1ece09683b3ed2c3c6497754
cch <cuichong@vip.126.com>
2016-01-11 16:43:01 +0800

    初始版本

---
+++ Fuhua/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fuhua.MvcApplication" Language="C#" %>
---
+++ Fuhua/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fuhua.Startup))]
+namespace Fuhua
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
